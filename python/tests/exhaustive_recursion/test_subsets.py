66d26e398e35fc297c96b9f21987778a6217245f
baroncurtin2 <knowledge3@gmail.com>
2023-01-03 21:05:51 -0500

    python permutations

---
+++ python/tests/exhaustive_recursion/test_subsets.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+
+import pytest
+
+from structy.exhaustive_recursion.subsets import subsets, subsets_with_tail_call_optimization
+
+
+@dataclass(slots=True)
+class SubsetsTestCase:
+    elements: list[str]
+    expected: list[list[str]]
+
+
+@pytest.fixture
+def test_cases() -> list[SubsetsTestCase]:
+    return [
+        SubsetsTestCase(
+            elements=["a", "b"],
+            expected=[[], ["b"], ["a"], ["a", "b"]],
+        ),
+        SubsetsTestCase(
+            elements=["a", "b", "c"],
+            expected=[[], ["c"], ["b"], ["b", "c"], ["a"], ["a", "c"], ["a", "b"], ["a", "b", "c"]],
+        ),
+        SubsetsTestCase(
+            elements=["x"],
+            expected=[[], ["x"]],
+        ),
+        SubsetsTestCase(
+            elements=[],
+            expected=[[]],
+        ),
+        SubsetsTestCase(
+            elements=["q", "r", "s", "t"],
+            expected=[
+                [],
+                ["t"],
+                ["s"],
+                ["s", "t"],
+                ["r"],
+                ["r", "t"],
+                ["r", "s"],
+                ["r", "s", "t"],
+                ["q"],
+                ["q", "t"],
+                ["q", "s"],
+                ["q", "s", "t"],
+                ["q", "r"],
+                ["q", "r", "t"],
+                ["q", "r", "s"],
+                ["q", "r", "s", "t"],
+            ],
+        ),
+    ]
+
+
+def test_subsets(test_cases: list[SubsetsTestCase]) -> None:
+    for t in test_cases:
+        assert subsets(t.elements) == t.expected
+
+
+def test_subsets_with_tail_call_optimization(test_cases: list[SubsetsTestCase]) -> None:
+    for t in test_cases:
+        assert subsets_with_tail_call_optimization(t.elements) == t.expected
